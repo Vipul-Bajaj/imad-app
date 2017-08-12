7abe63ffdda4d3f380447905165490b6d9d89ccb
Vipul Bajaj <vipulbjj@gmail.com>
2017-08-12 19:27:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var button = document.getElementById('counter');
-counter=0;
+
 button.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
@@ -7,7 +7,7 @@ button.onclick = function(){
           if(request.status === 200){
               var counter = request.responseText;
               var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+              span.innerHTML = counter.toString();
           }
       }  
     };
