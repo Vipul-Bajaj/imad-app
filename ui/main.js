4f328ab925a346072d7f2be13cad275883339f07
Vipul Bajaj <vipulbjj@gmail.com>
2017-08-12 20:24:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function(){
 
 var nameInput = documnet.getElementById('name');
 var name = nameInput.value;
-var submit = document.getElementById('submit-btn');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
   var names = ['name1','name2','name3'];
   var list='';
