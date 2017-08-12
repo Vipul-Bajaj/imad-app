0209d1f7a1bb1a610b39eef22911d5064310aa2c
Vipul Bajaj <vipulbjj@gmail.com>
2017-08-12 19:18:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,6 @@
-console.log('Loaded!');
-
-var img = document.getElementById('img');
-var marginLeft = 0; 
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + "px";
+var button = document.getElementById('counter');
+button.onclick = function(){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-img.onclick = function () {
-    var interval = setInterval(moveRight,50);
-};
