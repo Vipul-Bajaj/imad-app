91d4bb7389ab60324304a5de5b9af9bb12736fc4
Vipul Bajaj <vipulbjj@gmail.com>
2017-09-12 20:24:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
+var path = require('path');var pool = require('pg').Pool; 
 
 var app = express();
 app.use(morgan('combined'));
+
+var config = {
+    user:'vipulbjj',
+    database:'vipulbjj',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:'db-vipulbjj-32429'
+}
 
 var articles = {
     'article-one':{
@@ -79,6 +87,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/test-db', function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.status(200).send(JSON.stringify(results.rows));
+        }
+    })
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
